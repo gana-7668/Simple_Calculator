d2146cb46a8764dba546c056225488538f860df1
Ganesh Jadhav <136150686+gana-7668@users.noreply.github.com>
2024-02-16 22:14:53 +0530

    calculator_simple.py

---
+++ calculator_simple.py
@@ -0,0 +1,27 @@
+first=input("enter first number:")
+operator=input(" enter operator(+,-,%,*,/),:")
+second=input("enter the second  number:")
+
+first=int(first)
+second=int(second)
+
+
+if operator =="+":
+    print(first + second)
+
+elif operator =="-":
+    print(first - second)
+
+elif operator =="/":
+    print(first / second)
+
+elif operator =="*":
+    print(first * second)
+
+elif operator =="%":
+    print(first + second)
+
+
+else:
+
+    print("invalid")
